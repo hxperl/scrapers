dc5d2774cefa62208c9559db83bc950872f5128f
Unknown <hxperl@gmail.com>
2016-12-31 22:14:41 +0900

    11번가, 옥션 완성//g마켓 미완성

---
+++ src/category_11st.py
@@ -0,0 +1,54 @@
+import requests
+import re
+from lxml import html
+
+response = requests.get('http://www.11st.co.kr/html/inc_footer_data.js')
+
+assert response.status_code == 200
+
+#print(response.content)
+pattern = """("CtgrLv":3,"RefCtgrNo":\d+)"""
+#카테고리 레벨 3인것만 골라내기
+r = re.compile(pattern)
+#패턴 설정
+
+match = r.findall(response.text)
+#찾기
+
+match = list(set(match))
+#중복제거
+
+#print(match)
+
+for i in match:
+    i = i.replace("\"CtgrLv\":3,\"RefCtgrNo\":", "") #하위 카테고리 링크만 추출
+    #print(i)
+    #print(type(i))
+    try:
+        print('\t')
+        response = requests.get('http://www.11st.co.kr/html/category/'+ i + '.html')
+        #하위 링크 get요청
+        assert response.status_code == 200
+        #print(response.status_code)
+        tree=html.fromstring(response.content)
+        #하위 링크 카테고리 불러오기
+        j = tree.xpath("//h3[@class='tit']")
+        print(j[0].text + "\n")
+        for k in tree.xpath("//h4[@class='s_tit']/a"):
+            print("         "+k.text)
+            #print("         "+k.get('href'))
+            try:
+                response = requests.get(k.get('href'))
+                assert response.status_code==200
+                tree=html.fromstring(response.content)
+                c=1
+                for l in tree.xpath("//span[@class='txt_cat']"):
+                    if(c==1):
+                        c+=1
+                        continue
+                    print("                     "+l.text)
+            except:
+                pass
+
+    except:
+        pass
---
+++ src/category_auction.py
@@ -0,0 +1,54 @@
+import requests
+import re
+from lxml import html
+
+response = requests.get('http://script.auction.co.kr/common/HeaderCategoryInfo.js?_=1482923172564')
+
+assert response.status_code == 200
+
+#print(response.content)
+pattern = "HeaderCategoryT\('\d{2}0{6}', '.*'"
+#hash table 3번째 트리부터 탐색
+r = re.compile(pattern)
+#패턴 설정
+match = r.findall(response.text)
+#찾기
+match = list(set(match))
+
+
+
+for i in match:
+   #print(i)
+
+   hash_key = i.replace("HeaderCategoryT('", "")
+   hash_key = hash_key[:2]
+   category = i.replace("HeaderCategoryT('", "")
+   category = category[12:]
+   category = category.replace("', 'A'", "")
+   #print(hash_key)
+   print(category)
+
+   pattern2 = "HeaderCategoryT\('"+hash_key+"\d{6}', '.*'"
+   r2 = re.compile(pattern2)
+   match2 = r2.findall(response.text)
+   match2 = list(set(match2))
+   for j in match2:
+       hash_key2 = j.replace("HeaderCategoryT('", "")
+       hash_key2 = hash_key2[:8]
+       if hash_key2[2]=='0' and hash_key2[3]=='0':
+           continue
+       category2 = j.replace("HeaderCategoryT('", "")
+       category2 = category2[12:]
+       category2 = category2.replace("', 'A'", "")
+
+       print("         "+category2)
+
+       try:
+           response2 = requests.get('http://listings.auction.co.kr/category/list.aspx?category='+hash_key2)
+           assert response2.status_code==200
+           tree = html.fromstring(response2.content)
+           #print(response.status_code)
+           for k in tree.xpath("//ul[@class='group_list']/li/a"):
+               print("                      "+k.text)
+       except:
+           pass
