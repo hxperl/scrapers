ed6789aaf18ce0dc462734734ce821a4f46e64bd
Unknown <hxperl@gmail.com>
2017-01-01 00:44:02 +0900

    gmarket소스 수정

--- src/category_gmarket.py
+++ src/category_gmarket.py
@@ -10,15 +10,15 @@ print("****G마켓 카테고리****")
 for i in tree.xpath("//div[@id='main_gnb']/ul/li/a"):
     print(i.text)
     for j in i.xpath("../div/div/dl/dt"):
-        print("     ┣━━━━━━"+j.text)
+        print("         "+j.text)
         for k in j.xpath("../dd/a"):
-            print("     ┃           ┣━━━━━"+k.text)
+            print("                 "+k.text)
             try:
                 response2 = requests.get(k.get('href'))
                 assert response2.status_code==200
                 tree2 = html.fromstring(response2.content)
                 for l in tree2.xpath("//div[@class='mid-all-list']/div/h3/a"):
-                    print("     ┃           ┃           ┣━━━━"+l.text)
+                    print("                         "+l.text)
                     href = l.get('href')
                     ccp = href[:33]
                     ccp = ccp[25:]
@@ -33,6 +33,6 @@ for i in tree.xpath("//div[@id='main_gnb']/ul/li/a"):
                         for n in match2:
                             hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')
                             result = hangul.sub('', n)
-                            print("     ┃           ┃           ┃               ━━━━"+result)
+                            print("                                 "+result)
             except:
                 pass
