f824197fc7e77a8a6a26e424a90c0971243971f3
Unknown <hxperl@gmail.com>
2017-02-02 09:52:26 +0900

    에러 수정

--- src/new_order_scraping/site_new_order.py
+++ src/new_order_scraping/site_new_order.py
@@ -1,11 +1,16 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import requests
+
 
 class SiteNewOrder:
+
     def __init__(self, _id, _password):         # set ID, Password and Firefox webdriver
         self.driver = webdriver.Firefox()
         self._id = _id
         self._password = _password
+
     def getNewOrderNum(self):                   # pass
         pass
 
@@ -19,24 +24,15 @@ class SiteNewOrder:
             except Exception as e:
                 pass
 
-    def listAppend(self, _list, _item, depth=0):    # 주문 목록 리스트를 저장 하는 method [[list1],[list2]......]
-        if depth==0:
-            _item = []
-            _list.append(_item)
-        elif depth==1:
-            _list[-1].append(_item)
-        else:
-            print('depth error')
-            return
-        return _list
-
-    def sendBackspace(self, element, i, act):               # element에 backspace 키를 여러번 설정하고 ActionChains에 저장
+    # element에 backspace 키를 여러번 설정하고 ActionChains에 저장
+    def sendBackspace(self, element, i, act):
         for j in range(i):
             act.send_keys_to_element(element, Keys.BACK_SPACE)
         return act
 
-    def postTo(self, url, site_name, num):                  #테스트용
-        r = requests.post(url, json={site_name:num})
+    def postTo(self, url, site_name, num):  # 테스트용
+        r = requests.post(url, json={site_name: num})
+
     def driverQuit(self):                                   # webdriver 해제
         self.driver.quit()
 
