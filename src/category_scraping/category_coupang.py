432ae375182ccb3dfc3715acfef6ed17e3b70cc0
hxperl <hxperl@gmail.com>
2017-02-09 12:53:34 +0900

    Readme update

--- src/category_scraping/category_coupang.py
+++ src/category_scraping/category_coupang.py
@@ -1,30 +1,31 @@
 import requests
-import re
 from lxml import html
 
 response = requests.get('http://www.coupang.com/')
-assert response.status_code==200
-tree=html.fromstring(response.content)
+assert response.status_code == 200
+tree = html.fromstring(response.content)
 
 print("****쿠팡 카테고리****")
 for i in tree.xpath("//select[@class='search_category_filter']/option"):
-    if i.text=='전체':
+    if i.text == '전체':
         continue
     else:
         print(i.text)
         try:
-            response2 = requests.get('http://www.coupang.com/'+i.get('value'))
-            assert response2.status_code==200
-            tree2=html.fromstring(response2.content)
+            response2 = requests.get(
+                'http://www.coupang.com/' + i.get('value'))
+            assert response2.status_code == 200
+            tree2 = html.fromstring(response2.content)
             for j in tree2.xpath("//ul[@class='nav-items']/li/a"):
-                print("         "+j.text.strip())
+                print("         " + j.text.strip())
 
                 try:
-                    response3 = requests.get('http://www.coupang.com/'+j.get('href'))
-                    assert response3.status_code==200
+                    response3 = requests.get(
+                        'http://www.coupang.com/' + j.get('href'))
+                    assert response3.status_code == 200
                     tree3 = html.fromstring(response3.content)
                     for k in tree3.xpath("//li[@class='nav-item']/a"):
-                        print("                      "+k.text.strip())
+                        print("                      " + k.text.strip())
                 except:
                     pass
 
